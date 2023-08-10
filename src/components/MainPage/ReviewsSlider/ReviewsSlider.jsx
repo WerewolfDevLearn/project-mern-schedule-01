8c015927681b1754e6f6614fe42cab043ae26366
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-08-10 13:31:14 +0300

    fix toggler

--- src/components/MainPage/ReviewsSlider/ReviewsSlider.jsx
+++ src/components/MainPage/ReviewsSlider/ReviewsSlider.jsx
@@ -1,3 +1,6 @@
+import { useGetReviewsQuery } from '../../../redux/reviews/reviewsApi';
+
 export default function ReviewsSlider() {
+  const { data: reviews = [] } = useGetReviewsQuery();
   return <h2>ReviewsSlider</h2>;
 }
