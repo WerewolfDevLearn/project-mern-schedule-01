cbe8e3e451e5260aa8e0150cf03df2a66eff5342
Malberee <82259650+Malberee@users.noreply.github.com>
2023-08-11 16:11:55 +0300

    add icons component
    🐷

---
+++ src/components/Icons/ArrowDown.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+const SvgArrowDown = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="1em"
+		height="1em"
+		viewBox="0 0 32 32"
+		{...props}
+	>
+		<path
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={3.556}
+			d="m8 12 8 8 8-8"
+		/>
+	</svg>
+)
+export default SvgArrowDown
---
+++ src/components/Icons/Login.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+const SvgLogin = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="1em"
+		height="1em"
+		viewBox="0 0 32 32"
+		{...props}
+	>
+		<path
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={3.2}
+			d="M20 4h1.6c2.24 0 3.36 0 4.216.436a3.996 3.996 0 0 1 1.748 1.748C28 7.04 28 8.16 28 10.4v11.2c0 2.24 0 3.36-.436 4.216a4.001 4.001 0 0 1-1.748 1.748C24.96 28 23.84 28 21.6 28H20M13.333 9.333 20 16m0 0-6.667 6.667M20 16H4"
+		/>
+	</svg>
+)
+export default SvgLogin
