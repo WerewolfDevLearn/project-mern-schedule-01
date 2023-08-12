64d9c1e2a15d5d2208f4e1ff44364ce997e94752
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-08-12 08:47:33 +0300

    add header title changes dynamicly

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,24 +1,41 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { useThemeColors } from 'src/components/MainLayout/ThemeToggler/ThemeContextProvider';
 import { ThemeProvider } from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 import Loader from '../shared/Loader/Loader';
-import { usePHBState } from '../../redux/selectors';
+import { useisLoading, useisRefreshing } from '../../redux/selectors';
 import Container from '../shared/Container';
+import { getCurrent } from '../../redux/auth/authOps';
 
+import { LocationContext } from './LocationContext';
 import SideBar from './SideBar/SideBar';
 import AppHeader from './AppHeader/AppHeader';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
-  const { isLoading } = usePHBState();
+  const isRefreshing = useisRefreshing();
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const activePage = location.pathname.split('/')[1];
+
   const theme = useThemeColors().theme;
-  return (
+  useEffect(() => {
+    dispatch(getCurrent());
+  }, [dispatch]);
+  const isLoading = useisLoading();
+  return isRefreshing ? (
+    <Loader />
+  ) : (
     <ThemeProvider theme={theme}>
       <Container>
-        <AppHeader />
-        <SideBar />
+        <LocationContext.Provider value={activePage}>
+          <AppHeader />
+          <SideBar />
+        </LocationContext.Provider>
         {isLoading ? <Loader /> : <Outlet />}
         <ToastContainer />
       </Container>
