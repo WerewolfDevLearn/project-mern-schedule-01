62a7d8fb824398730274a527424c1f42122f77b9
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-08-15 09:10:00 +0300

    remove Navigate from registerPage

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+
 import { useState } from 'react';
 import logo from 'src/images/others/desktop/goose-quote1x.png';
 import logo2x from 'src/images/others/desktop/goose-quote2x.png';
@@ -14,20 +14,10 @@ import { Container, ContentWrap, Wrap, StyledImg } from './RegisterPage.styled';
 
 export default function RegisterPage() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const currentDay = new Date();
-  const [emailSend, setEmailSend] = useState(false);
-  const error = useError();
-  const isLoading = useisLoading();
 
   const callBack = (data) => {
     dispatch(register(data));
-    setEmailSend(true);
   };
-
-  if (!error && !isLoading && emailSend) {
-    navigate(`/calendar/month/${currentDay}`);
-  }
 
   return (
     <Container>
--- src/services/authAxApi.js
+++ src/services/authAxApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://project-mern-schedule-03.onrender.com/api';
+axios.defaults.baseURL = 'http://localhost:3001/api';
 
 export async function userRegister(userData) {
   const response = await axios.post('/users/register', userData);
