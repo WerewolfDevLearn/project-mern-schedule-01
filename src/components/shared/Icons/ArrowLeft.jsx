623259dc4bc611b1e0c61a25d53cd4ae4b1b2fa0
Malberee <82259650+Malberee@users.noreply.github.com>
2023-08-17 22:07:27 +0300

    add some icons

---
+++ src/components/shared/Icons/ArrowLeft.jsx
@@ -0,0 +1,12 @@
+const SvgArrowLeft = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="6" height="12" {...props}>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1.5"
+      d="M5.25 10.5.75 6l4.5-4.5"
+    />
+  </svg>
+);
+export default SvgArrowLeft;
---
+++ src/components/shared/Icons/ArrowRight.jsx
@@ -0,0 +1,12 @@
+const SvgArrowRight = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="6" height="12" {...props}>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1.5"
+      d="M.75 10.5 5.25 6 .75 1.5"
+    />
+  </svg>
+);
+export default SvgArrowRight;
