5f60e567dd49fe3d53d8b6d8163ab5d615c20170
Malberee <82259650+Malberee@users.noreply.github.com>
2023-08-17 22:08:59 +0300

    fix

--- src/components/shared/Icons/ArrowRight.jsx
+++ src/components/shared/Icons/ArrowRight.jsx
@@ -1,5 +1,5 @@
 const SvgArrowRight = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="6" height="12" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="6" height="12" fill="none" {...props}>
     <path
       stroke="currentColor"
       stroke-linecap="round"
