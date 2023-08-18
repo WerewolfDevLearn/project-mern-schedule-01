6922ce559943ffac8032cafc78b8b3a146ffc6b5
Malberee <82259650+Malberee@users.noreply.github.com>
2023-08-18 14:11:12 +0300

    fix

--- src/components/CalendarPage/ChoosedDay/ChoosedDay.jsx
+++ src/components/CalendarPage/ChoosedDay/ChoosedDay.jsx
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types';
 import { getYear, getMonth, parse } from 'date-fns';
 import { useParams } from 'react-router-dom';
 import { useGetTasksQuery } from 'src/redux/tasks/tasksApi';
+import Loader from 'src/components/shared/Loader/Loader';
 
 import TasksColumnsList from './TasksColumnsList/TasksColumnsList';
 import DayCalendarHead from './DayCalendarHead/DayCalendarHead';
@@ -15,7 +15,7 @@ export default function ChoosedDay() {
   const { data: tasks, isFetching, isLoading } = useGetTasksQuery(date);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loader />;
   }
 
   return (
