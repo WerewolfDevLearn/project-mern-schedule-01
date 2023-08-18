9e832d33ee568e0c4a6c5076f5277b3eda1aa32d
Malberee <82259650+Malberee@users.noreply.github.com>
2023-08-18 15:18:06 +0300

    Update Loader.jsx

--- src/components/shared/Loader/Loader.jsx
+++ src/components/shared/Loader/Loader.jsx
@@ -1,11 +1,13 @@
 import { Puff } from 'react-loader-spinner';
 
+import gooseLoader from 'src/images/others/gooseLoader.gif';
+
 import { DivloaderContainer } from '../../Styled-Components/Styled-Components.styled';
 
 export default function Loader() {
   return (
     <DivloaderContainer>
-      <img src="/src/images/others/gooseLoader.gif" />
+      <img src={`${gooseLoader}`} />
 
       {/* <Puff
         height="80"
