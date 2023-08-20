263f8d766a0a3b02a0cd5819bf0a9478508a025a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-21 00:46:39 +0300

    clear width

--- src/components/MainLayout/UserInfo/UserInfo.styled.jsx
+++ src/components/MainLayout/UserInfo/UserInfo.styled.jsx
@@ -4,7 +4,6 @@ export const UserInfoStyles = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 84px;
 `;
 export const UserName = styled.p`
   color: ${({ theme }) => theme.colors.textCancelBtn};
