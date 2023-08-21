ebe9a8f058fb5684dde03888a1b4868aa8240092
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-21 14:52:07 +0300

    change flex

--- src/components/CalendarPage/ChoosedDay/TasksColumnsList/TasksColumnsList.styled.jsx
+++ src/components/CalendarPage/ChoosedDay/TasksColumnsList/TasksColumnsList.styled.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const TasksColumnsListStyles = styled.ul`
   display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
 
   @media screen and (width >= 768px) {
     gap: 16px;
